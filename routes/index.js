d76c55b57ceed6469e5b18ac4a881e1391bef2e6
dor balmas <dorbal31@gmail.com>
2020-08-05 20:12:33 +0300

    05 08 20

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+let dataOnlyReleventStatus;
+let array = [];
+// http://localhost:3000/product
+router.get("/", (req, res) => {
+  let url =
+    "https://msbit-exam-products-store.firebaseio.com/deliveryProducts/products.json";
+  axios.get(url).then((resp) => {
+    let myData = resp.data;
+    dataOnlyReleventStatus = myData.filter((item) => {
+      if (item.status) {
+        if (item.status == 1) {
+          return item;
+        }
+      }
+    });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json(dataOnlyReleventStatus);
+  });
+});
+
+module.exports = router;
