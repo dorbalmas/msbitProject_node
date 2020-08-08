532e7cb2cd4f30a3265da21ebd2497842850913b
dor balmas <dorbal31@gmail.com>
2020-08-08 18:23:18 +0300

    06082020

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,47 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
+let dataOnlyReleventStatus = [];
+let tempMyData = [];
 // http://localhost:3000/product
 router.get("/", (req, res) => {
   let url =
     "https://msbit-exam-products-store.firebaseio.com/deliveryProducts/products.json";
   axios.get(url).then((resp) => {
     let myData = resp.data;
-    let dataOnlyReleventStatus = myData.filter((item) => {
+    myData.map((item) => {
       if (item.status) {
         if (item.status == 1) {
-          return item;
+          if (item.type == 1) dataOnlyReleventStatus.push(item.fedex);
+          else if (item.type == 2) {
+            for (let i = 0; i < item.ups.length; i++) {
+              dataOnlyReleventStatus.push(item.ups[i]);
+              item.ups[i].status = 1;
+              item.ups[i].type = 2;
+            }
+          } else if (item.type == 3) dataOnlyReleventStatus.push(item);
         }
       }
     });
     res.header("Access-Control-Allow-Origin", "*");
     res.json(dataOnlyReleventStatus);
+    tempMyData = dataOnlyReleventStatus;
+    console.log(dataOnlyReleventStatus);
+    dataOnlyReleventStatus = [];
   });
+});
+// http://localhost:3000/product/search/?q=
+router.get("/search/", (req, res) => {
+  const mySearch = req.query.q;
+  console.log(tempMyData);
+  let dataSearch = tempMyData.filter(
+    (item) =>
+      item.name.includes(mySearch.toLocaleLowerCase()) ||
+      item.description.includes(mySearch.toLocaleLowerCase())
+  );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(dataSearch);
+  console.log(dataSearch);
 });
 
 module.exports = router;
